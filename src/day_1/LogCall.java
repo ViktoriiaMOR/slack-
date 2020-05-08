cd3030ccf129ceea5e1b513f3305ef9e99cf6860
ViktoriiaMOR <64219552+ViktoriiaMOR@users.noreply.github.com>
2020-05-08 15:31:54 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,5 +3,6 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("Another practice!");
+        System.out.println("/n Another practice!");//added from git hub
     }
 }
